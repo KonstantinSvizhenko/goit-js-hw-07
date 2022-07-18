1980467ecbf794058e44a353bc844d7b081f6b2d
KonstantinSvizhenko <kostua.90@gmail.com>
2022-07-18 20:15:54 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery')
+const gallaryList = makeGalleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', gallaryList)
+
+function makeGalleryItems(items) {
+    return items.map(({preview, original, description}) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href='${original}'>
+         <img
+      class="gallery__image"
+      src='${preview}' 
+      data-source='${original}
+      alt='${description}'
+    />
+  </a>
+</div>`
+    })
+    .join('');
+}
+ 
+galleryContainer.addEventListener('click', openModal);
+
+function openModal(e) {
+    e.preventDefault();
+    
+    if (e.target.nodeName !== 'IMG') {
+        return
+    }
+    const instance = basicLightbox.create(`
+    <img src=${e.target.dataset.source} width="800" height="600" alt=${e.target.alt}>
+`)
+instance.show()
+}
+
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery')
+const gallaryList = makeGalleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', gallaryList)
+
+function makeGalleryItems(items) {
+    return items.map(({preview, original, description}) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href='${original}'>
+         <img
+      class="gallery__image"
+      src='${preview}' 
+      data-source='${original}
+      alt='${description}'
+    />
+  </a>
+</div>`
+    })
+    .join('');
+}
+const galleryBox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt'
+
+});
+
 console.log(galleryItems);
